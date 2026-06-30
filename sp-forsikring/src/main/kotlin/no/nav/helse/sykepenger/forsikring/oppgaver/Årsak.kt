77f6be2cb70f40107d8866b045575a02d5e52173
Martin Remøy Solheim <3852471+MartinMekk@users.noreply.github.com>
2026-06-30 13:34:17 +0200

    🚧 Legg til GosysOppgaveClient for å kunne opprette oppgaver
    Koden er hentet fra spre-forsikringsoppgaver
    
    Co-authored-by: Sivert Lunsæter <sivert.frang.lunseter@nav.no>

---
+++ sp-forsikring/src/main/kotlin/no/nav/helse/sykepenger/forsikring/oppgaver/Årsak.kt
@@ -0,0 +1,9 @@
+package no.nav.helse.sykepenger.forsikring.oppgaver
+
+import java.math.BigDecimal
+
+sealed interface Årsak {
+    object UtbetaltFraDagÉnOgDekningsgrad80Prosent : Årsak
+    object SykepengerettOpphørtPåGrunnAvMaksdatoAlderEllerDød : Årsak
+    data class ForStortAvvikMellomSykepengegrunnlagOgPremiegrunnlag(val sykepengegrunnlag: BigDecimal, val premiegrunnlag: BigDecimal, val avviksprosent: BigDecimal) : Årsak
+}
