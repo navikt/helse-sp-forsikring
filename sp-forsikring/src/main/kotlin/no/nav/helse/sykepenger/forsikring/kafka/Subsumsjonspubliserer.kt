85d1fc35a1358c80229f71c5e986e556032c6227
Nourhan Ragab <16683497+nurrebass1@users.noreply.github.com>
2026-09-16 11:04:21 +0200

    🚚 Flytter Subsumsjonspubliserer til kafka mappen
    Co-authored-by: Trine Linderud <trine.linderud@nav.no>
    Co-authored-by: Martin Remøy Solheim <3852471+martinmekk@users.noreply.github.com>
    Co-authored-by: Svein Elgstøen <189128153+navelgen@users.noreply.github.com>
    Co-authored-by: Eirik Gallefoss <eirik.gallefoss@nav.no>

--- sp-forsikring/src/main/kotlin/no/nav/helse/sykepenger/forsikring/kafka/Subsumsjonspubliserer.kt
+++ sp-forsikring/src/main/kotlin/no/nav/helse/sykepenger/forsikring/kafka/Subsumsjonspubliserer.kt
@@ -3,10 +3,6 @@ package no.nav.helse.sykepenger.forsikring.subsumsjon
 import no.nav.helse.sykepenger.forsikring.domain.Forsikringsvurdering
 import java.util.*
 
-/**
- * Publiserer subsumsjonene som følger av en forsikringsvurdering. Grensesnittet holder kunnskapen om rapiden
- * utenfor API-et og forretningslogikken, slik at de kun forholder seg til at subsumsjoner skal publiseres.
- */
 interface Subsumsjonspubliserer {
     fun publiser(
         forsikringsvurdering: Forsikringsvurdering,
